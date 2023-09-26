546feaf659955c41cd43cd296dc5758d7c846b08
alexa <alexakola4eva@gmail.com>
2023-09-27 00:02:31 +0600

    author and genre crud

--- BookLovers/settings.py
+++ BookLovers/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)0it@@q_ug%+m3%bd1!fo2l6ykh3and@_^o-)tyr91-g($&4sn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
