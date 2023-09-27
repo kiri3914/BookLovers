545f5b16f786ab0987fce2f84595632909ef05da
kiri3914 <t.bektur.2002@gmail.com>
2023-09-27 11:54:49 +0600

    final commit

--- apps/books/forms.py
+++ apps/books/forms.py
@@ -6,3 +6,9 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author', 'genre', 'cover_image', 'description']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'author': forms.TextInput(attrs={'class': 'form-control'}),
+            'cover_image': forms.FileInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+        }
